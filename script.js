c29b017858e68ab8f7a32e5d1b6cc8d0f9c8a693
elbedie <diego.ddvid@gmail.com>
2024-11-17 22:39:56 -0300

    Alterando cores

--- script.js
+++ script.js
@@ -4,25 +4,15 @@ const corTres = document.getElementById("tres")
 
 
 function alterarModoNoturno(){
-    corBody.style.backgroundColor = "#3C3D37";
-    corBody.style.color = "#FAF7F0";
+    corBody.style.backgroundImage = "linear-gradient(to right top, #16161a, #1c1c1f, #222225, #29292a, #2f2f30)";
+    corBody.style.color = "#fffffe";
     const links = document.querySelectorAll("a");
     links.forEach(link => link.style.color = "#FAF7F0");    
-    corDois.style.backgroundColor = "#FAF7F0";
-    corDois.style.color = "#4A4947";
-    corTres.style.backgroundColor = "#FAF7F0";
-    corTres.style.color = "#4A4947";
 }
 
 function alterarModoDiurno() {
-    corBody.style.backgroundColor = "#FAF7F0"; // Fundo claro
-    corBody.style.color = "#3C3D37"; // Texto escuro
+    corBody.style.backgroundImage = "radial-gradient(circle, #fffffe, #f5f7f5, #eaeeee, #e1e6e7, #d9dde1)";
+    corBody.style.color = "#3C3D37";
     const links = document.querySelectorAll("a");
-    links.forEach(link => link.style.color = "#FAF7F0"); // Links escuros
-    
-    corDois.style.backgroundColor = "#3C3D37"; // Seção "dois" com fundo escuro
-    corDois.style.color = "#FAF7F0"; // Texto claro
-
-    corTres.style.backgroundColor = "#3C3D37"; // Seção "três" com fundo escuro
-    corTres.style.color = "#FAF7F0"; // Texto claro
+    links.forEach(link => link.style.color = "#3C3D37"); 
 }
